79d676af7faab6013ef287ad8b9da0a8953ca1a7
s19358 <s19358@pjwstk.edu.pl>
2020-03-20 13:42:29 +0100

    tutorial2

---
+++ tutorial2/tutorial2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace tutorial2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student() { firstname = "Aysenur", lastname = "Ozgur", snumber = "s19358" };
+            FileStream writer = new FileStream(@"data.xml", FileMode.Create);
+            XmlSerializer serializer = new XmlSerializer(typeof(Student), new XmlRootAttribute("university"));
+
+            serializer.Serialize(writer, student);
+
+            Console.WriteLine("Hello World!");
+
+
+        }
+    }
+}
---
+++ tutorial2/tutorial2/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace tutorial2
+{
+     public class Student
+    {
+
+        [XmlElement(ElementName = "fname")]
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string snumber { get; set; }
+    }
+}
